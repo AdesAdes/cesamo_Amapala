6ac7cfbdeb00922c88262d50c3ff7a789472da06
darioaplicano <alex_dario92@hotmail.com>
2015-06-14 17:22:20 -0600

    Creación del login

--- index.php
+++ index.php
@@ -7,11 +7,52 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>CESAMO AMAPALA</title>
+        <link href="bootstrap-3.3.4-dist/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="bootstrap-3.3.4-dist/css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <link href="bootstrap-3.3.4-dist/css/estiloPersonal.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container well" id="contenedorLogin" >
+            <form class="form-horizontal">
+                <div class="container">
+                    <img src="Images/profileImage_origin.png" id="imagenLogin" alt="" class="img-circle"/>
+                </div>
+              <div class="form-group">
+                <label for="usuario" class="col-sm-4 contro-label">Usuario</label>
+                <div class="col-sm-8">
+                  <div class="input-group">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                    <input type="text" class="form-control" id="usuarioLogin">
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="password" class="col-sm-4 ontro-label">Contraseña</label>
+                <div class="col-sm-8">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                        <input type="password" class="form-control" id="contrasenaLogin">
+                    </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> Recuerdame
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-success">Iniciar Sesion</button>
+                </div>
+              </div>
+            </form>
+        </div>
+        <script src="bootstrap-3.3.4-dist/js/jquery-1.11.3.min.js" type="text/javascript"></script>
+        <script src="bootstrap-3.3.4-dist/js/bootstrap.js" type="text/javascript"></script>
     </body>
 </html>
